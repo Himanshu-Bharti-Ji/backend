7f33090e2bb982d93179093677bdc94a26bc1708
Himasnhu <himanshubhartimax@gmail.com>
2024-03-02 15:44:47 +0530

    setup initial files and folders and start the sever

---
+++ index.js
@@ -0,0 +1,13 @@
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+app.use("/", (req, res) => {
+    res.send("Hello from Hi Tech Himanshu's Server")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+})
