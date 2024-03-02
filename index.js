3d44279b3b1a7fac4c2fa579c536c60138aa4f36
Himasnhu <himanshubhartimax@gmail.com>
2024-03-02 18:49:58 +0530

    Connect to  Database

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const { log } = require("console");
 const express = require("express");
+const dbConnect = require("./config/dbConnect.js");
+const { error } = require("console");
 const app = express();
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: ".env" });
 const PORT = process.env.PORT || 4000;
 
-app.use("/", (req, res) => {
-    res.send("Hello from Hi Tech Himanshu's Server")
+// I add app.use and app.listen in try catch 
+dbConnect()
+.then(() => {
+    app.use("/", (req, res) => {
+        res.send("Hello from Hi Tech Himanshu's Server")
+    })
+    
+    app.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo DB connection failed !!! ", error);
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
-})
